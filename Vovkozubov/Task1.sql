9bce06892a79c78a568d46ea877716586a71d115
Slava <slavok.v005@mail.ru>
2024-06-25 12:34:57 +0200

    new

---
+++ Vovkozubov/Task1.sql
@@ -0,0 +1,10 @@
+-- Вывести фамилии менеджеров, у которых есть заказы, исп. механизм вложенных запросов
+SELECT
+	LastName
+FROM Employees
+
+WHERE
+	EmployeeID IN (
+    SELECT DISTINCT EmployeeID
+    FROM Orders
+)
